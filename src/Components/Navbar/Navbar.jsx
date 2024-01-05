29b30483867957397c9379bfdcd862be493c2fd7
Varad Sandeep Naik <varad0007@gmail.com>
2024-01-05 15:46:07 +0530

    Made the website basic mobile responsive and added a new mbile navbar . Nav Menu links doesnt work yet

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 
+import MenuButton from './Assets/Icons/menu.png'
+import CloseButton from './Assets/Icons/close.png'
+
+
+
 const Navbar = () => {
+
+  const [mobileNav , setMobileNav] = useState("Mobile-Nav-notActive");
+
   return (
     <div>
+        <div className="Nav-Menus-Mobile" id={mobileNav}>
+          <div className="Nav-Mobile-Menu-Wrapper">
+            <a href="">home.</a>
+            <a href="">work.</a>
+            <a href="">contact.</a>
+          </div>
+          <div className="Nav-Mobile-Menu-Button">
+            <button onClick={() => {setMobileNav("Mobile-Nav-notActive")}}><img src={CloseButton}/></button>
+          </div>   
+        </div>
         <div className="Nav-Wrapper">
             <div className="Nav-Brand">
                 <p>varad naik.</p>
@@ -12,6 +30,9 @@ const Navbar = () => {
                 <a href="">home.</a>
                 <a href="">work.</a>
                 <a href="">contact.</a>
+            </div>
+            <div className="Nav-Menu-Wrapper-Mobile">
+              <button onClick={() => {setMobileNav("Mobile-Nav-Active")}}><img src={MenuButton}/></button>
             </div>
         </div>
     </div>
